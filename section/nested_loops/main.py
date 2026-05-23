e1df757187156a1bb111153df85ff68f18adc026
shubhammarcus <>
2026-05-23 06:38:03 +0000

    Nested loops

---
+++ section/nested_loops/main.py
@@ -0,0 +1,17 @@
+# List of trips
+trips = [['france', 'germany', 'italy', 'spain', 'netherlands', 'sweden', 'norway', 'switzerland', 'austria', 'portugal', 'belgium'],
+         ['japan', 'china', 'thailand', 'vietnam', 'ndonesia', 'india', 'malaysia', 'philippines', 'singapore', 'mongolia'],
+         ['usa', 'canada', 'mexico', 'brazil', 'argentina', 'colombia', 'peru', 'chile', 'ecuador'],
+         ['egypt', 'morocco', 'south africa', 'tunisia', 'algeria', 'kenya', 'nigeria', 'ethiopia'],
+         ['australia', 'new zealand', 'fiji', 'papua new guinea', 'samoa']]
+
+# List of all countries 
+countries = []
+for list in trips:
+    for country in list:
+        new = country.capitalize()
+        countries.append(new)
+
+
+# Testing
+print('List of Countries:', countries)
