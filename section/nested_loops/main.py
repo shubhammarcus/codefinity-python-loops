f8887f78d3984ab7fcac5c404206539b107085cf
shubhammarcus <>
2026-05-23 07:42:37 +0000

    Nested Loops

--- section/nested_loops/main.py
+++ section/nested_loops/main.py
@@ -1,17 +1,32 @@
-# List of trips
-trips = [['france', 'germany', 'italy', 'spain', 'netherlands', 'sweden', 'norway', 'switzerland', 'austria', 'portugal', 'belgium'],
-         ['japan', 'china', 'thailand', 'vietnam', 'ndonesia', 'india', 'malaysia', 'philippines', 'singapore', 'mongolia'],
-         ['usa', 'canada', 'mexico', 'brazil', 'argentina', 'colombia', 'peru', 'chile', 'ecuador'],
-         ['egypt', 'morocco', 'south africa', 'tunisia', 'algeria', 'kenya', 'nigeria', 'ethiopia'],
-         ['australia', 'new zealand', 'fiji', 'papua new guinea', 'samoa']]
+# List of travel costs (each sublist represents a trip)
+travel_costs = [
+    [5, 15, 10, 8, 25, 30, 55, 68, 75, 5],
+    [60, 20, 60, 70, 80, 80, 80, 90, 90, 90],
+    [100, 100, 100, 100, 50, 110, 110, 120, 120, 120, 130],
+    [130, 140, 39, 140, 150, 150, 150, 150, 160, 160],
+    [170, 180, 180, 190, 40, 190, 200],
+    [200, 200, 200, 210, 11, 220, 220, 220, 250, 250, 250],
+    [280, 300, 300, 110, 300, 320, 350, 400, 400, 450],
+    [480, 500, 500, 90, 500, 550, 600, 700]
+]
 
-# List of all countries 
-countries = []
-for list in trips:
-    for country in list:
-        new = country.capitalize()
-        countries.append(new)
+# List to store maximum costs per trip
+max_costs = []
 
+i = 0
+while i < len(travel_costs):
+    # Start with the first expense as current max
+    max_cost = travel_costs[i][0]
+    j = 1
+    # Iterate remaining expenses
+    while j < len(travel_costs[i]):
+        if travel_costs[i][j] > max_cost:
+            max_cost = travel_costs[i][j]
+        j += 1
+
+    # Append the highest expense found for this trip
+    max_costs.append(max_cost)
+    i += 1
 
 # Testing
-print('List of Countries:', countries)
+print('Maximum Travel Costs:', max_costs)
